fa85117d37dc571f7c8d7f4df3b34071dd889879
r05725018 <r05725018@ntu.edu.tw>
2016-11-18 05:15:50 +0000

    add index.html

---
+++ js/init.js
@@ -0,0 +1,30 @@
+
+skel.init({
+	prefix: 'css/style',
+	resetCSS: true,
+	boxModel: 'border',
+	grid: {
+		gutters: 50
+	},
+	breakpoints: {
+		
+		'desktop': {
+			range: '481-',
+			containers: 1200
+		},
+	
+	}
+}, {
+	panels: {
+		panels: {
+			navPanel: {
+				breakpoints: 'mobile',
+				position: 'left',
+				style: 'reveal',
+				size: '80%',
+				html: '<div data-action="navList" data-args="nav"></div>'
+			}
+		}
+		
+	}	
+});
